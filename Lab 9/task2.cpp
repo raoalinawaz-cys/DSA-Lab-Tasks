6d6ee4c1e1c192652a3a79280b41f683831b2707
ran <info@raoali.com>
2022-12-02 09:11:00 +0500

    Lab 9 task 1 task 2

---
+++ Lab 9/task2.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        next = NULL;
+    }
+};
+class Queue
+{
+public:
+    Node *front;
+    Node *rear;
+    Queue()
+    {
+        front = NULL;
+        rear = NULL;
+    }
+    Node *createNode()
+    {
+        Node *newnode = new Node();
+        return newnode;
+    }
+    void enqueue(int value)
+    {
+        Node *newnode = createNode();
+        newnode->data = value;
+        if (front == NULL && rear == NULL)
+        {
+            front = newnode;
+            return;
+        }
+        Node *temp = front;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        rear = newnode;
+    }
+    void dequeue()
+    {
+        if (front == NULL)
+        {
+            cout << "Queue is already empty";
+            return;
+        }
+        front = front->next;
+    }
+    void displayQueue()
+    {
+        Node *temp = front;
+        if (front == NULL)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+    void reverseDisplay(Node *temp)
+    {
+        if (temp == NULL)
+        {
+            return;
+        }
+        reverseDisplay(temp->next);
+        cout << temp->data << " ";
+    }
+};
+int main()
+{
+    Queue q;
+    q.enqueue(3);
+    q.enqueue(8);
+    q.enqueue(5);
+    q.enqueue(9);
+    q.enqueue(16);
+    // q.dequeue();
+    // q.dequeue();
+    q.displayQueue();
+    cout << endl
+         << "Reverse Order: ";
+    q.reverseDisplay(q.front);
+}
